bf26015b5beb6d75742ad62b0d3e7c9ef0922e62
hengkx <ycxzhkx@gmail.com>
2019-09-14 11:30:27 +0800

    Create plant.dart

---
+++ lib/utils/plant.dart
@@ -0,0 +1,27 @@
+class Plant {
+  static bool isPlant(Flower flower, Soil soil) {
+    if (soil == null) {
+      return true;
+    }
+    if (flower.combineId != null ||
+        (!flower.isMoneyBuy() && flower.count <= 0)) {
+      return false;
+    }
+    if (soil.type == 0) {
+      return flower.type == 2 ||
+          flower.type == 100 ||
+          (flower.type == 99 && soil.potLevel >= flower.potLevel);
+    } else if (soil.type == 1) {
+      return flower.type == 1 && soil.potLevel >= flower.potLevel;
+    } else if (soil.type == 3) {
+      if (soil.hanglevel == 4) {
+        return flower.type == 2 ||
+            flower.type == 3 ||
+            (flower.type == 99 && flower.season < 4);
+      } else {
+        return flower.type == 3 && flower.potLevel <= soil.hanglevel;
+      }
+    }
+    return false;
+  }
+}
